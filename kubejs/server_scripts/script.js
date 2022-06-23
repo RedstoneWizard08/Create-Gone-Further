ddbab33acb21f91cdf4fa0d0997ec2b020391eb7
Jacob Sapoznikow <jacob1coder@gmail.com>
2022-06-23 12:44:10 -0700

    Update things, remove some mods, add some mods

--- kubejs/server_scripts/script.js
+++ kubejs/server_scripts/script.js
@@ -4,3 +4,10 @@ settings.logAddedRecipes = true;
 settings.logRemovedRecipes = true;
 settings.logSkippedRecipes = false;
 settings.logErroringRecipes = true;
+
+onEvent("recipes", (event) => {
+    event.shaped("1x chisel:chisel", ["  X", " X ", "Y  "], {
+        X: "minecraft:diamond",
+        Y: "minecraft:stick",
+    });
+});
